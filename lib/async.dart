f57f8957f13aae03b4b05ecd06af2d92ba6cef85
Michael <michaelpoland2001@gmail.com>
2022-04-14 14:00:09 +0700

    async

--- lib/async.dart
+++ lib/async.dart
@@ -1,15 +1,52 @@
 void main() async {
-  funct1();
-//   await funct4(10).then(
-//     (value) {
-//       print(value);
-//       print("then");
-//     },
-//   ).catchError((error) {
-//     print(error);
-//     print("error");
-//   });
+//   funct1();
+// //   await funct4(10).then(
+// //     (value) {
+// //       print(value);
+// //       print("then");
+// //     },
+// //   ).catchError((error) {
+// //     print(error);
+// //     print("error");
+// //   });
   
+//   try{
+//   var value =   await funct4(30);
+//     print (value);
+//       print("try");
+//   } catch(error){
+//      print(error);
+//     print("error");
+//   }
+//   funct2();
+//   funct3();
+// }
+
+// funct1() {
+//   print("function 1");
+// }
+
+// funct2() {
+//   print("function 2");
+// }
+
+// funct3() {
+//   print("function 3");
+// }
+
+// Future<String> funct4(int nilai) {
+//   return Future.delayed(Duration(seconds: 2), () {
+// //     int nilai;
+
+//     if (nilai >= 50) {
+//       return "Lulus";
+//     } else {
+//       throw "Tidak Lulus";
+//     }
+// //     return "Hitung Nilai";
+//   });
+
+funct1(); 
   try{
   var value =   await funct4(30);
     print (value);
@@ -19,7 +56,15 @@ void main() async {
     print("error");
   }
   funct2();
-  funct3();
+  await funct4(20).then(
+    (value) {
+      print(value);
+      print("then");
+    },
+  ).catchError((error) {
+    print(error);
+    print("error");
+  });
 }
 
 funct1() {
@@ -30,8 +75,14 @@ funct2() {
   print("function 2");
 }
 
-funct3() {
-  print("function 3");
+Future<String> funct3(int nilaii) {
+  return Future.delayed(Duration(seconds: 2), () {
+     if (nilaii <= 30) {
+      return "Lulus";
+    } else {
+      throw "Tidak Lulus";
+    }
+  });
 }
 
 Future<String> funct4(int nilai) {
